09d90d3ce630184fbb01765eb221341009116ccd
flash <sachinkotian007@yahoo.com>
2020-02-03 23:29:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import PropTypes from 'prop-types'
 
 import axios from "axios";
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    showMessage:false
   };
+ static propTypes={
+}
   async componentDidMount() {
     this.setState({ loading: true });
     const res = await axios.get(
@@ -18,18 +22,28 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 searchUsers=async( text)=>{ 
+  if (text.length>0) {
+   
+  this.setState({ loading: true,showMessage: false });
+
   const res = await axios.get(
     `https://api.github.com/search/users?q=${text} & client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
   );
-  this.setState({ users: res.data.items, loading: false });
-
+  this.setState({ users: res.data.items, loading: false ,});
+ 
+  }
+  else {  this.setState({ showMessage: true });
+}
+}
+clearUsers=()=>{
+  this.setState({users: [], loading: false})
 }
   render() {
     return (
       <div className="App">
         <Navbar title=" Gitfinder.io" icon="fa fa-github" />
         <div className="container">
-        <Search searchUsers={this.searchUsers} />
+        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length>0?true:false}  showMessage={this.state.showMessage}   />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
  class Search extends Component {
      state={
          text:''
      }
-     
+     static propTypes={
+        searchUsers:PropTypes.func.isRequired,
+        clearUser:PropTypes.func.isRequired,
+    showClear:PropTypes.bool.isRequired,
+    }
      submitForm=(e)=>{ 
    e.preventDefault();
    this.props.searchUsers(this.state.text)
@@ -17,7 +22,10 @@ import React, { Component } from 'react'
 <form className="form" onSubmit={this.submitForm}>
     <input type="text" input="text" placeholder="search" value={this.state.text} onChange={this.onChange}/>
     <input type="submit" value="Search" className="btn btn-dark btn-block" />
-</form>                
+    {this.props.showMessage && <h4 className='text-center text-danger my'>Please enter a username!!</h4>}
+
+</form> {this.props.showClear&&  <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>}
+              
             </div>
         )
     }
